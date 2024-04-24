6331b5b697c84ca590488788c66ddd0c1a52cd6f
Filipe <filipelehmannp@gmail.com>
2024-04-24 20:52:18 -0300

    feat: add basic routing to login page from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export default function Home() {
+  const router = useRouter();
+  const isAuthenticated = false;
+
+  useEffect(() => {
+    // Logica de autenticação usando localStorage
+
+    if (!isAuthenticated) {
+      router.push('/login');
+    }
+  }, []);
+
+  if (!isAuthenticated) return <div>Carregando...</div>;
+
   return <div>Main page</div>;
 }

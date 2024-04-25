83f54a16e80a93ead1f272840767398d90c7122e
Filipe <filipelehmannp@gmail.com>
2024-04-25 08:42:48 -0300

    chore: updates login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,66 @@
 'use client';
+import { login } from '@/api/auth/login';
+import { errorMessages } from '@/validation/validationErrors';
+import { zodResolver } from '@hookform/resolvers/zod';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const requiredInputSize = 1; // required fields in zod use the method .min(1)
+
+const logInSchema = z.object({
+  email: z
+    .string()
+    .min(requiredInputSize, { message: errorMessages().required })
+    .email({ message: errorMessages().email }),
+  password: z
+    .string()
+    .min(requiredInputSize, { message: errorMessages().required }),
+});
+
+export type LogInSchema = z.infer<typeof logInSchema>;
 
 export default function LogIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<LogInSchema>({
+    resolver: zodResolver(logInSchema),
+  });
+
+  const handleLogIn = (data: LogInSchema) => {
+    try {
+      const result = login(data);
+      if (result === 'ok') {
+        reset();
+        router.push('/');
+      } else if (result === 'email') {
+        setError('email', {
+          message: 'Email não cadastrado',
+        });
+      } else {
+        setError('password', {
+          message: 'Senha inválida',
+        });
+      }
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
   };
 
   return (
@@ -40,30 +80,37 @@ export default function LogIn() {
         <Typography component="h1" variant="h5">
           Login
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(handleLogIn)}
+          noValidate
+          sx={{ mt: 1 }}
+        >
           <TextField
             margin="normal"
             required
             fullWidth
             id="email"
             label="Endereço de Email"
-            name="email"
             autoComplete="email"
             autoFocus
+            disabled={isSubmitting}
+            error={Boolean(errors.email)}
+            helperText={errors.email?.message}
+            {...register('email')}
           />
           <TextField
             margin="normal"
             required
             fullWidth
-            name="password"
             label="Senha"
             type="password"
             id="password"
             autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Lembrar de mim"
+            disabled={isSubmitting}
+            error={Boolean(errors.password)}
+            helperText={errors.password?.message}
+            {...register('password')}
           />
           <Button
             type="submit"

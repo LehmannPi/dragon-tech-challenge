2ce5292c5a7e77b3fbca0dcbc5ba2524d6ae4d44
Filipe <filipelehmannp@gmail.com>
2024-04-25 11:59:57 -0300

    feat: add updated contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,125 +1,155 @@
-import { Grid, TextField } from '@mui/material';
+import { Box, Grid, TextField } from '@mui/material';
 
 const ContactForm = () => {
   return (
-    <form className="flex items-center gap-2">
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        id="Nome"
-        label="Nome"
-        name="Nome"
-        autoComplete="Nome"
-      />
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        id="cpf"
-        label="CPF"
-        name="cpf"
-        autoComplete="cpf"
-      />
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        id="telefone"
-        label="Telefone"
-        name="telefone"
-        autoComplete="telefone"
-      />
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        id="cep"
-        label="CEP"
-        name="cep"
-        autoComplete="cep"
-      />
-      <Grid container justifyContent="center">
-        <Grid item>
+    <Box component="form" onSubmit={() => {}} noValidate sx={{ p: 3 }}>
+      <Grid container spacing={2} rowSpacing={0.5}>
+        <Grid item xs={12} sm={6}>
           <TextField
             margin="normal"
             required
             fullWidth
+            variant="standard"
+            size="small"
+            id="Nome"
+            label="Nome"
+            name="Nome"
+            autoComplete="Nome"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            variant="standard"
+            size="small"
+            id="cpf"
+            label="CPF"
+            name="cpf"
+            autoComplete="cpf"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            variant="standard"
+            size="small"
+            id="telefone"
+            label="Telefone"
+            name="telefone"
+            autoComplete="telefone"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            variant="standard"
+            size="small"
+            id="cep"
+            label="CEP"
+            name="cep"
+            autoComplete="cep"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            variant="standard"
+            size="small"
             id="logradouro"
-            label="logradouro"
-            name="Logradouro"
+            label="Logradouro"
+            name="logradouro"
             autoComplete="logradouro"
           />
         </Grid>
-        <Grid item>
+        <Grid item xs={12} sm={6}>
           <TextField
             margin="normal"
             required
             fullWidth
+            variant="standard"
+            size="small"
             id="complemento"
             label="Complemento"
             name="complemento"
             autoComplete="complemento"
           />
         </Grid>
-        <Grid item>
+        <Grid item xs={12} sm={6}>
           <TextField
             margin="normal"
             required
             fullWidth
+            variant="standard"
+            size="small"
             id="bairro"
             label="Bairro"
             name="bairro"
             autoComplete="bairro"
           />
         </Grid>
-        <Grid item>
+        <Grid item xs={12} sm={6}>
           <TextField
             margin="normal"
             required
             fullWidth
+            variant="standard"
+            size="small"
             id="localidade"
             label="Cidade"
             name="localidade"
             autoComplete="localidade"
           />
         </Grid>
-        <Grid item>
+        <Grid item xs={12} sm={6}>
           <TextField
             margin="normal"
             required
             fullWidth
+            variant="standard"
+            size="small"
             id="uf"
             label="Estado"
             name="uf"
             autoComplete="uf"
           />
         </Grid>
+        <Grid item xs={12}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            variant="standard"
+            size="small"
+            type="number"
+            id="latitude"
+            label="Latitude"
+            name="latitude"
+            autoComplete="latitude"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            variant="standard"
+            size="small"
+            type="number"
+            id="longitude"
+            label="Longitude"
+            name="longitude"
+            autoComplete="longitude"
+          />
+        </Grid>
       </Grid>
-
-      <Grid container justifyContent="flex-end">
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          type="number"
-          id="latitude"
-          label="latitude"
-          name="latitude"
-          autoComplete="latitude"
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          type="number"
-          id="latitude"
-          label="latitude"
-          name="latitude"
-          autoComplete="latitude"
-        />
-      </Grid>
-    </form>
+    </Box>
   );
 };
 
